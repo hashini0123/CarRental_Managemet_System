e376703f9468fa1f60289d7a4c079c27587d9cbc
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-10 21:49:48 +0530

    car rental #11

--- src/main/java/edu/icet/model/dto/CarDTO.java
+++ src/main/java/edu/icet/model/dto/CarDTO.java
@@ -1,4 +1,18 @@
 package edu.icet.model.dto;
 
 public class CarDTO {
+    private String carID;
+
+    private String model;
+
+    private String brand;
+
+    private String fuelType;
+
+    private int seatingCapacity;
+
+    private double dailyRate;
+
+    private String status;
+
 }
