8def9a93d7496ec4f868ea29025a6639c8879eb6
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-10 22:02:19 +0530

    car rental #14

--- src/main/java/edu/icet/model/dto/ReportDTO.java
+++ src/main/java/edu/icet/model/dto/ReportDTO.java
@@ -2,4 +2,12 @@ package edu.icet.model.dto;
 
 public class ReportDTO {
 
+    private String reportType;
+
+    private int totalRentals;
+
+    private double totalRevenue;
+
+    private double utilizationRate;
+
 }
