94056995d085374ab3ace3815459ed648b02ce9e
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-10 21:58:29 +0530

    car rental #13

--- src/main/java/edu/icet/model/dto/PaymentDTO.java
+++ src/main/java/edu/icet/model/dto/PaymentDTO.java
@@ -1,4 +1,17 @@
 package edu.icet.model.dto;
 
+import java.time.LocalDate;
+
 public class PaymentDTO {
+
+    private String paymentID;
+
+    private String bookingID;
+
+    private double amount;
+
+    private LocalDate paymentDate;
+
+    private String paymentMethod;
+
 }
