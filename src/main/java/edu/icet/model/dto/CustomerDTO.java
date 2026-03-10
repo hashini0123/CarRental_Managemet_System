220e7e7b0387938bb11bc7340f3c5501875f64ba
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-10 21:46:43 +0530

    car rental #10

--- src/main/java/edu/icet/model/dto/CustomerDTO.java
+++ src/main/java/edu/icet/model/dto/CustomerDTO.java
@@ -1,4 +1,14 @@
 package edu.icet.model.dto;
 
 public class CustomerDTO {
+    private String custID;
+
+    private String custName;
+
+    private String email;
+
+    private String password;
+
+    private String contactNumber;
+
 }
