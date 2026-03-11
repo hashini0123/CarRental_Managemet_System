2349447efe8fff2d15f9d5096419861334663d46
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-11 09:05:25 +0530

    car rental create booking package & class #22

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,5 +1,31 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.CustomerDTO;
+import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public boolean addCustomer(@RequestBody CustomerDTO customerDTO){
+        return customerService.addCustomer(customerDTO);
+    }
+
+    public boolean uppdateCustomer(@RequestBody CustomerDTO customerDTO){
+        return customerService.updateCustomer(customerDTO);
+    }
+
+    public boolean deleteByID(String ID){
+        return customerService.deleteByID(ID);
+    }
+
+
+
 
 }
