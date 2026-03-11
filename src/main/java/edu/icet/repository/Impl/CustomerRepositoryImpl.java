420c51597cb098af9d261d2147dfe86b540f447f
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-11 17:01:19 +0530

    car rental #29

--- src/main/java/edu/icet/repository/Impl/CustomerRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/CustomerRepositoryImpl.java
@@ -1,4 +1,35 @@
 package edu.icet.repository.Impl;
 
-public class CustomerRepositoryImpl {
+import edu.icet.model.dto.CustomerDTO;
+import edu.icet.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class CustomerRepositoryImpl implements CustomerRepository {
+    @Override
+    public boolean addCustomer(CustomerDTO customerDTO) {
+
+        //String sql = "INSERT INTO customer value"
+
+        return false;
+    }
+
+    @Override
+    public boolean updateCustomer(CustomerDTO customerDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteByID(String id) {
+        return false;
+    }
+
+    @Override
+    public List<CustomerDTO> getAll() {
+        return List.of();
+    }
 }
