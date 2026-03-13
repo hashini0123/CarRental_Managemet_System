91fc203546e9c35e1ed428b1a1ae87b3ba2ab6ea
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-13 10:24:23 +0530

    car rental #32

--- src/main/java/edu/icet/repository/CustomerRepository.java
+++ src/main/java/edu/icet/repository/CustomerRepository.java
@@ -2,10 +2,11 @@ package edu.icet.repository;
 
 import edu.icet.model.dto.CustomerDTO;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface CustomerRepository {
-    boolean addCustomer(CustomerDTO customerDTO);
+    boolean addCustomer(CustomerDTO customerDTO) throws SQLException;
 
     boolean updateCustomer(CustomerDTO customerDTO);
 
