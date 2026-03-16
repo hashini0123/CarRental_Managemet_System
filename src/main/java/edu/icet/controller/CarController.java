5ac95f4c4a3be706eb8aaad500b612d12d5ebd2c
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-16 22:52:30 +0530

    car rental #46

--- src/main/java/edu/icet/controller/CarController.java
+++ src/main/java/edu/icet/controller/CarController.java
@@ -1,5 +1,36 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.CarDTO;
+import edu.icet.service.CarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/car")
 public class CarController {
 
+    private final CarService carService;
+
+    @PostMapping("/add")
+    public boolean addCar(@RequestBody CarDTO carDTO){
+        return carService.addCar(carDTO);
+    }
+
+    @PutMapping("/update")
+    public boolean updateCar(@RequestBody CarDTO carDTO){
+        return carService.updateCar(carDTO);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public boolean deleteCar(@PathVariable String id){
+        return carService.deleteById(id);
+    }
+
+    @GetMapping("/get")
+    public List<CarDTO> getAll(){
+        return carService.getAll();
+    }
 }
