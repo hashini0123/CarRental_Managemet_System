1b2bee119493826be984c5eae44d06a2686e0d50
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-16 23:36:39 +0530

    car rental #50

--- src/main/java/edu/icet/repository/CarRepository.java
+++ src/main/java/edu/icet/repository/CarRepository.java
@@ -1,4 +1,15 @@
 package edu.icet.repository;
 
+import edu.icet.model.dto.CarDTO;
+
+import java.util.List;
+
 public interface CarRepository {
+    boolean addCar(CarDTO carDTO);
+
+    boolean updateCar(CarDTO carDTO);
+
+    boolean deleteById(String id);
+
+    List<CarDTO> getAll();
 }
