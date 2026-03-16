da2f785b7ab9656a22293ae01e19ceec0fc98e90
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-16 23:36:53 +0530

    car rental #51

--- src/main/java/edu/icet/repository/Impl/CarRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/CarRepositoryImpl.java
@@ -1,4 +1,36 @@
 package edu.icet.repository.Impl;
 
-public class CarRepositoryImpl {
+import edu.icet.model.dto.CarDTO;
+import edu.icet.repository.CarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CarRepositoryImpl implements CarRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public boolean addCar(CarDTO carDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updateCar(CarDTO carDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return false;
+    }
+
+    @Override
+    public List<CarDTO> getAll() {
+        return List.of();
+    }
 }
