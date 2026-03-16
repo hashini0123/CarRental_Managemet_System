47ef2863bd091bcbdd859416f1ca0f52551b0fd9
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-16 23:37:05 +0530

    car rental #52

--- src/main/java/edu/icet/service/Impl/CarServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/CarServiceImpl.java
@@ -1,4 +1,36 @@
 package edu.icet.service.Impl;
 
-public class CarServiceImpl {
+import edu.icet.model.dto.CarDTO;
+import edu.icet.repository.CarRepository;
+import edu.icet.service.CarService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CarServiceImpl implements CarService {
+
+    private final CarRepository carRepository;
+
+    @Override
+    public boolean addCar(CarDTO carDTO) {
+        return carRepository.addCar(carDTO);
+    }
+
+    @Override
+    public boolean updateCar(CarDTO carDTO) {
+        return carRepository.updateCar(carDTO);
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return carRepository.deleteById(id);
+    }
+
+    @Override
+    public List<CarDTO> getAll() {
+        return carRepository.getAll();
+    }
 }
