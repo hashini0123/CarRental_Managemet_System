f83eed5d821fbf276ff38b8205e0d2635b36df0e
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-16 22:53:03 +0530

    car rental #48

--- src/main/java/edu/icet/service/CarService.java
+++ src/main/java/edu/icet/service/CarService.java
@@ -1,4 +1,16 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.CarDTO;
+
+import java.util.List;
+
 public interface CarService {
+    
+    boolean addCar(CarDTO carDTO);
+
+    boolean updateCar(CarDTO carDTO);
+
+    boolean deleteById(String id);
+
+    List<CarDTO> getAll();
 }
