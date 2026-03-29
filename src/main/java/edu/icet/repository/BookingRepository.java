e575615efb1335fc7d3768932c0f6001b9080298
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-29 23:21:37 +0530

    commit #57

--- src/main/java/edu/icet/repository/BookingRepository.java
+++ src/main/java/edu/icet/repository/BookingRepository.java
@@ -1,4 +1,15 @@
 package edu.icet.repository;
 
+import edu.icet.model.dto.BookingDTO;
+
+import java.util.List;
+
 public interface BookingRepository {
+    boolean addBook(BookingDTO bookingDTO);
+
+    boolean updateBook(BookingDTO bookingDTO);
+
+    boolean deleteById(String id);
+
+    List<BookingDTO> getAll();
 }
