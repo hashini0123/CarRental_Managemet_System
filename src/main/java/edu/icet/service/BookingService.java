8949577bfc5738f937c3bfa3b6e230b8be50fc9c
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-29 23:21:47 +0530

    commit #58

--- src/main/java/edu/icet/service/BookingService.java
+++ src/main/java/edu/icet/service/BookingService.java
@@ -1,5 +1,16 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.BookingDTO;
+
+import java.util.List;
+
 public interface BookingService
 {
+    boolean addBook(BookingDTO bookingDTO);
+
+    boolean updateBook(BookingDTO bookingDTO);
+
+    boolean deleteById(String id);
+
+    List<BookingDTO> getAll();
 }
