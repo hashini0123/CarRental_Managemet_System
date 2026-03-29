b5b979f3571c7364987200df38c68e3ad8882003
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-29 23:21:26 +0530

    commit #56

--- src/main/java/edu/icet/controller/BookingController.java
+++ src/main/java/edu/icet/controller/BookingController.java
@@ -1,12 +1,36 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.BookingDTO;
+import edu.icet.service.BookingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/booking")
 @RequiredArgsConstructor
 public class BookingController {
 
+    private final BookingService bookingService;
+
+    @PostMapping("/add")
+    public boolean addBook(@RequestBody BookingDTO bookingDTO){
+        return bookingService.addBook(bookingDTO);
+    }
+
+    @PutMapping("/update")
+    public boolean updateBook(@RequestBody BookingDTO bookingDTO){
+        return bookingService.updateBook(bookingDTO);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public boolean deleteBook(@PathVariable String id){
+        return bookingService.deleteById(id);
+    }
+
+    @GetMapping("/get")
+    public List<BookingDTO> getAll(){
+        return bookingService.getAll();
+    }
 }
