75ce306cb18f79e6db3f01cbc3558f60b59db8d0
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-29 23:21:59 +0530

    commit #59

--- src/main/java/edu/icet/service/Impl/BookingServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/BookingServiceImpl.java
@@ -1,4 +1,36 @@
 package edu.icet.service.Impl;
 
-public class BookingServiceImpl {
+import edu.icet.model.dto.BookingDTO;
+import edu.icet.repository.BookingRepository;
+import edu.icet.service.BookingService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class BookingServiceImpl implements BookingService {
+
+    public final BookingRepository bookingRepository;
+
+    @Override
+    public boolean addBook(BookingDTO bookingDTO) {
+        return bookingRepository.addBook(bookingDTO);
+    }
+
+    @Override
+    public boolean updateBook(BookingDTO bookingDTO) {
+        return bookingRepository.updateBook(bookingDTO);
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return bookingRepository.deleteById(id);
+    }
+
+    @Override
+    public List<BookingDTO> getAll() {
+        return bookingRepository.getAll();
+    }
 }
