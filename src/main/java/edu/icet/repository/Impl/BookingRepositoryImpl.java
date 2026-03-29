0cb144f579826fd9dcc74d0550dd8e02bb1f18ba
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-29 23:23:29 +0530

    commit #60

--- src/main/java/edu/icet/repository/Impl/BookingRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/BookingRepositoryImpl.java
@@ -1,4 +1,28 @@
 package edu.icet.repository.Impl;
 
-public class BookingRepositoryImpl {
+import edu.icet.model.dto.BookingDTO;
+import edu.icet.repository.BookingRepository;
+
+import java.util.List;
+
+public class BookingRepositoryImpl implements BookingRepository {
+    @Override
+    public boolean addBook(BookingDTO bookingDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updateBook(BookingDTO bookingDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return false;
+    }
+
+    @Override
+    public List<BookingDTO> getAll() {
+        return List.of();
+    }
 }
