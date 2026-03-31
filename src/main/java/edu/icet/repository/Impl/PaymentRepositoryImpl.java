5936e38ca529e21a29cc8e97f6c61eee68b61c75
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-31 22:56:57 +0530

    commit #68

--- src/main/java/edu/icet/repository/Impl/PaymentRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/PaymentRepositoryImpl.java
@@ -1,4 +1,28 @@
 package edu.icet.repository.Impl;
 
-public class PaymentRepositoryImpl {
+import edu.icet.model.dto.PaymentDTO;
+import edu.icet.repository.PaymentRepository;
+
+import java.util.List;
+
+public class PaymentRepositoryImpl implements PaymentRepository {
+    @Override
+    public boolean addPayment(PaymentDTO paymentDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updtePayment(PaymentDTO paymentDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return false;
+    }
+
+    @Override
+    public List<PaymentDTO> getAll() {
+        return List.of();
+    }
 }
