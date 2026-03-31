dab95ddac86a6a8ea0334f8777faedf7bb43ce82
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-31 22:57:07 +0530

    commit #69

--- src/main/java/edu/icet/service/Impl/PaymentServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/PaymentServiceImpl.java
@@ -1,4 +1,36 @@
 package edu.icet.service.Impl;
 
-public class PaymentServiceImpl {
+import edu.icet.model.dto.PaymentDTO;
+import edu.icet.repository.PaymentRepository;
+import edu.icet.service.PaymentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    @Override
+    public boolean addPayment(PaymentDTO paymentDTO) {
+         return paymentRepository.addPayment(paymentDTO);
+    }
+
+    @Override
+    public boolean updatePayment(PaymentDTO paymentDTO) {
+        return paymentRepository.updtePayment(paymentDTO);
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return paymentRepository.deleteById(id);
+    }
+
+    @Override
+    public List<PaymentDTO> getAll() {
+        return paymentRepository.getAll();
+    }
 }
