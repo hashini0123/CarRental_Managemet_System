b313e96405b506612c7a7d3c9cc389112b39188f
hashini0123 <nanayakkarahashini5@gmail.com>
2026-03-31 22:49:16 +0530

    commit #66

--- src/main/java/edu/icet/service/PaymentService.java
+++ src/main/java/edu/icet/service/PaymentService.java
@@ -1,4 +1,15 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.PaymentDTO;
+
+import java.util.List;
+
 public interface PaymentService {
+    boolean addPayment(PaymentDTO paymentDTO);
+
+    boolean updatePayment(PaymentDTO paymentDTO);
+
+    boolean deleteById(String id);
+
+    List<PaymentDTO> getAll();
 }
