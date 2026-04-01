5b50c29ca210b332f6bd9366dbd06cad29816f82
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-01 12:28:36 +0530

    commit #70

--- src/main/java/edu/icet/repository/Impl/PaymentRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/PaymentRepositoryImpl.java
@@ -2,13 +2,28 @@ package edu.icet.repository.Impl;
 
 import edu.icet.model.dto.PaymentDTO;
 import edu.icet.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+@RequiredArgsConstructor
 public class PaymentRepositoryImpl implements PaymentRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public boolean addPayment(PaymentDTO paymentDTO) {
-        return false;
+        String sql = "INSERT INTO payments VALUE (?,?,?,?,?)";
+        return jdbcTemplate.update(sql,
+                paymentDTO.getPaymentID(),
+                paymentDTO.getBookingID(),
+                paymentDTO.getAmount(),
+                paymentDTO.getPaymentDate(),
+                paymentDTO.getPaymentMethod()
+                )>0;
     }
 
     @Override
