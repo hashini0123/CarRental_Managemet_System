8cfc285dbcf06243edce03f395c92046e4618776
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-02 20:14:38 +0530

    commit #80

--- src/main/java/edu/icet/service/ReportService.java
+++ src/main/java/edu/icet/service/ReportService.java
@@ -1,4 +1,15 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.ReportDTO;
+
+import java.util.List;
+
 public interface ReportService {
+    boolean addReport(ReportDTO reportDTO);
+
+    boolean updateReport(ReportDTO reportDTO);
+
+    boolean deleteById(String id);
+
+    List<ReportDTO> getAll();
 }
