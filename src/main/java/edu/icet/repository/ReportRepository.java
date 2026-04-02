ddffd85a2229052967d388c6c58a1adea22e8027
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-02 20:14:14 +0530

    commit #78

--- src/main/java/edu/icet/repository/ReportRepository.java
+++ src/main/java/edu/icet/repository/ReportRepository.java
@@ -1,4 +1,15 @@
 package edu.icet.repository;
 
+import edu.icet.model.dto.ReportDTO;
+
+import java.util.List;
+
 public interface ReportRepository {
+    boolean addReport(ReportDTO reportDTO);
+
+    boolean updateReport(ReportDTO reportDTO);
+
+    boolean deleteById(String id);
+
+    List<ReportDTO> getAll();
 }
