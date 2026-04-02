9029e4414558608507e5ed35a5e92a9d4931e333
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-02 20:14:51 +0530

    commit #81

--- src/main/java/edu/icet/service/Impl/ReportServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/ReportServiceImpl.java
@@ -1,4 +1,36 @@
 package edu.icet.service.Impl;
 
-public class ReportServiceImpl {
+import edu.icet.model.dto.ReportDTO;
+import edu.icet.repository.ReportRepository;
+import edu.icet.service.ReportService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ReportServiceImpl implements ReportService {
+
+    private final ReportRepository reportRepository;
+
+    @Override
+    public boolean addReport(ReportDTO reportDTO) {
+        return reportRepository.addReport(reportDTO);
+    }
+
+    @Override
+    public boolean updateReport(ReportDTO reportDTO) {
+        return reportRepository.updateReport(reportDTO);
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return reportRepository.deleteById(id);
+    }
+
+    @Override
+    public List<ReportDTO> getAll() {
+        return reportRepository.getAll();
+    }
 }
