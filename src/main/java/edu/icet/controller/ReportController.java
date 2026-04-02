116de353543894e759a27dc37064853752a08e71
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-02 20:13:57 +0530

    commit #77

--- src/main/java/edu/icet/controller/ReportController.java
+++ src/main/java/edu/icet/controller/ReportController.java
@@ -1,4 +1,36 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.ReportDTO;
+import edu.icet.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/report")
+@RequiredArgsConstructor
 public class ReportController {
+
+    private final ReportService reportService;
+
+    @PostMapping("/add")
+    public boolean addReport(@RequestBody ReportDTO reportDTO){
+        return reportService.addReport(reportDTO);
+    }
+
+    @PutMapping("/update")
+    public boolean updateReport(@RequestBody ReportDTO reportDTO){
+        return reportService.updateReport(reportDTO);
+    }
+
+    @DeleteMapping("delete-by-id/{id}")
+    public boolean deleteById (@PathVariable String id){
+        return reportService.deleteById(id);
+    }
+
+    @GetMapping("/get")
+    public List<ReportDTO> getAll(){
+        return reportService.getAll();
+    }
 }
