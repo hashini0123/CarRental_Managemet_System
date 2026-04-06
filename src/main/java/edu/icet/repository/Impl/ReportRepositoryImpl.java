c65a21d05f47e90e3b0464c5f68e9a25c0ef6de1
hashini0123 <nanayakkarahashini5@gmail.com>
2026-04-06 23:38:52 +0530

    commit #88

--- src/main/java/edu/icet/repository/Impl/ReportRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/ReportRepositoryImpl.java
@@ -2,15 +2,28 @@ package edu.icet.repository.Impl;
 
 import edu.icet.model.dto.ReportDTO;
 import edu.icet.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class ReportRepositoryImpl implements ReportRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public boolean addReport(ReportDTO reportDTO) {
-        return false;
+        String sql = "INSERT INTO reports VALUES (?,?,?,?)";
+        return jdbcTemplate.update(sql,
+                reportDTO.getReportType(),
+                reportDTO.getTotalRentals(),
+                reportDTO.getTotalRevenue(),
+                reportDTO.getUtilizationRate()
+                )>0;
+
     }
 
     @Override
