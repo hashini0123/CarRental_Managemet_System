cc3611d53f19e34e2119a3c12e149fd48064436d
hashini0123 <nanayakkarahashini5@gmail.com>
2026-05-26 23:51:22 +0530

    Add isCarAvailable method to BookingRepository for availability checks

--- src/main/java/edu/icet/repository/BookingRepository.java
+++ src/main/java/edu/icet/repository/BookingRepository.java
@@ -2,6 +2,7 @@ package edu.icet.repository;
 
 import edu.icet.model.dto.BookingDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BookingRepository {
@@ -12,4 +13,11 @@ public interface BookingRepository {
     boolean deleteById(String id);
 
     List<BookingDTO> getAll();
+
+    boolean isCarAvailable(
+            Long carId,
+            LocalDate startDate,
+            LocalDate endDate
+    );
+
 }
