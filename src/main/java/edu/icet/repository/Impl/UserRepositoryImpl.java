b179b661136f98bacbde30aee2eb68053a8be733
hashini0123 <nanayakkarahashini5@gmail.com>
2026-05-28 19:50:34 +0530

    Refactor UserRepositoryImpl methods for user management

--- src/main/java/edu/icet/repository/Impl/UserRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/UserRepositoryImpl.java
@@ -8,67 +8,36 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Repository
+@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
     @Override
     public boolean addUser(UserDTO userDTO) {
-        String sql = "INSERT INTO users VALUES(?,?,?,?,?,?)";
-           return jdbcTemplate.update(sql,
-                   userDTO.getUserID(),
-                   userDTO.getUserName(),
-                   userDTO.getEmail(),
-                   userDTO.getPassword(),
-                   userDTO.getRole(),
-                   userDTO.getCreated_at()
-            ) > 0;
+        String sql = "INSERT INTO users VALUE (?,?,?,?,?,?)";
+        return jdbcTemplate.update(sql,
+                userDTO.getId(),
+                userDTO.getName(),
+                userDTO.getEmail(),
+                userDTO.getRole(),
+                userDTO.getCreated_at()
+                )>0;
     }
-
 
     @Override
     public boolean updateUser(UserDTO userDTO) {
-
-        String sql = "UPDATE user SET userName=?, email=?, password=?, role=? created_at=? WHERE user_id=?";
-
-        return jdbcTemplate.update(sql,
-                userDTO.getUserID(),
-                userDTO.getUserName(),
-                userDTO.getEmail(),
-                userDTO.getPassword(),
-                userDTO.getRole(),
-                userDTO.getCreated_at()
-        )>0;
+        return false;
     }
 
     @Override
     public boolean deleteByID(String id) {
-
-        String sql = "DELETE FROM users  WHERE user_id=? ";
-
-        return jdbcTemplate.update(sql,id)>0;
+        return false;
     }
 
     @Override
     public List<UserDTO> getAll() {
-
-        String sql = "SELECT * FROM users";
-
-        List<UserDTO> userDTOList = jdbcTemplate.query(sql,(rs, rowNum) -> {
-
-            UserDTO userDTO = new UserDTO();
-
-            userDTO.setUserID(rs.getLong(1));
-            userDTO.setUserName(rs.getString(2));
-            userDTO.setEmail(rs.getString(3));
-            userDTO.setPassword(rs.getInt(4));
-            userDTO.setRole(rs.getString(5));
-            userDTO.setCreated_at(rs.getString(6));
-            return userDTO;
-        });
-
-        return userDTOList;
+        return List.of();
     }
 }
